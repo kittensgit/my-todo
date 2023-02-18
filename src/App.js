bfe0cadfc409df5e7a1547a341cff8610c9eaa27
Nika <nikaonime@gmail.com>
2023-02-18 13:25:51 +0200

    1 day

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
+import { useState } from 'react'
+import Todo from './Todo'
+import TodoForm from './TodoForm'
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2, 9),
+        task: userInput,
+        complete: false
+      }
+      setTodos([...todos, newItem])
+    }
+  }
+
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)])
+  }
+
+  const handleToggle = (id) => {
+    setTodos([
+      ...todos.map((todo) =>
+        todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
+      )
+    ])
+  }
+
   return (
-    <div></div>
+    <div className="App">
+      <header>
+        <h1>todo list: {todos.length}</h1>
+      </header>
+      <TodoForm addTask={addTask} />
+      {todos.map((todo) => {
+        return (
+          <Todo
+            todo={todo}
+            key={todo.id}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+          />
+        )
+      })}
+    </div>
   );
 }
 
