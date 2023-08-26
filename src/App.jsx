acab732038b363424c9847473f74b646ea9b5c28
nika <nikaonime@gmail.com>
2023-08-26 21:23:03 +0300

    clear project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import TodoForm from './components/TodoForm';
-import Todo from './components/Todo';
 import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
@@ -9,44 +7,7 @@ const App = () => {
         { id: uuidv4(), tasks: 'to make dinner', complete: false },
     ]);
 
-    const addTodo = (tasks) => {
-        const newTodo = {
-            id: uuidv4(),
-            tasks: tasks,
-            complete: false,
-        };
-        setTodos([newTodo, ...todos]);
-    };
-
-    const deleteTodo = (id) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-    };
-
-    const toggleTodo = (id) => {
-        let newTodo = [...todos].filter((todo) => {
-            if (todo.id === id) {
-                todo.complete = !todo.complete;
-            }
-            return todo;
-        });
-        setTodos(newTodo);
-    };
-
-    return (
-        <div className="App">
-            <TodoForm addTodo={addTodo} />
-            {todos.map((todo) => (
-                <Todo
-                    complete={todo.complete}
-                    toggleTodo={toggleTodo}
-                    deleteTodo={deleteTodo}
-                    key={todo.id}
-                    tasks={todo.tasks}
-                    id={todo.id}
-                />
-            ))}
-        </div>
-    );
+    return <div className="App">App</div>;
 };
 
 export default App;
