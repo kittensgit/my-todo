29f749ffeaf0b7b99ecc43d4339a7f66c5755b8e
nika <nikaonime@gmail.com>
2023-08-28 14:23:11 +0300

    toggleTodo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Todo = ({ tasks, id, removeTask }) => {
+const Todo = ({ tasks, id, removeTask, toggleTodo, complete }) => {
     return (
         <div className="todo">
-            {tasks}
+            <div
+                className={complete ? 'complete' : ''}
+                onClick={() => toggleTodo(id)}
+            >
+                {tasks}
+            </div>
             <button onClick={() => removeTask(id)}>delete</button>
         </div>
     );
