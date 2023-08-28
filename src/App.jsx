e736aeff8b25fe1fdc41f16f470afbe040d85899
nika <nikaonime@gmail.com>
2023-08-28 13:53:49 +0300

    reset project

--- src/App.jsx
+++ src/App.jsx
@@ -9,46 +9,7 @@ const App = () => {
         { id: uuidv4(), tasks: 'to make dinner', complete: false },
     ]);
 
-    const addTodo = (tasks) => {
-        const newTodo = {
-            id: uuidv4(),
-            tasks,
-            complete: false,
-        };
-        setTodos([...todos, newTodo]);
-    };
-
-    const deleteTodo = (id) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
-    };
-
-    const completeTodo = (id) => {
-        const newTodo = todos.filter((todo) => {
-            if (todo.id === id) {
-                todo.complete = !todo.complete;
-            }
-            return todo;
-        });
-        setTodos(newTodo);
-    };
-
-    return (
-        <div className="App">
-            <h1>Todo count: {todos.length}</h1>
-            <TodoForm addTodo={addTodo} />
-            {todos.map((todo) => (
-                <Todo
-                    key={todo.id}
-                    id={todo.id}
-                    todo={todo}
-                    complete={todo.complete}
-                    tasks={todo.tasks}
-                    deleteTodo={deleteTodo}
-                    completeTodo={completeTodo}
-                />
-            ))}
-        </div>
-    );
+    return <div>App</div>;
 };
 
 export default App;
